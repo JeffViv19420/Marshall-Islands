28c1b5b781ee0854693d1205cda22b71dc4b674b
JeffViv19420 <vivaanpr19@gmail.com>
2024-08-02 17:07:19 -0700

    playerSpeed

--- Marshall Islands/Assets/CameraMovement.cs
+++ Marshall Islands/Assets/CameraMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public float playerSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,22 +15,22 @@ public class CameraMovement : MonoBehaviour
     void Update()
     {
         if (Input.GetKey("w")){
-            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x-0.1f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x-0.1f*playerSpeed, transform.localEulerAngles.y, transform.localEulerAngles.z);
         }
         if (Input.GetKey("s")){
-            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x+0.1f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x+0.1f*playerSpeed, transform.localEulerAngles.y, transform.localEulerAngles.z);
         }
         if (Input.GetKey("a")){
-            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y-0.1f, transform.localEulerAngles.z);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y-0.1f*playerSpeed, transform.localEulerAngles.z);
         }
         if (Input.GetKey("d")){
-            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y+0.1f, transform.localEulerAngles.z);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y+0.1f*playerSpeed, transform.localEulerAngles.z);
         }
         if (Input.GetKey("q")){
-            transform.position += new Vector3(transform.forward.x*0.01f, transform.forward.y*0.01f, transform.forward.z*0.01f);
+            transform.position += new Vector3(transform.forward.x*0.01f*playerSpeed, transform.forward.y*0.01f*playerSpeed, transform.forward.z*0.01f*playerSpeed);
         }
         if (Input.GetKey("z")){
-            transform.position -= new Vector3(transform.forward.x*0.01f, transform.forward.y*0.01f, transform.forward.z*0.01f);
+            transform.position -= new Vector3(transform.forward.x*0.01f*playerSpeed, transform.forward.y*0.01f*playerSpeed, transform.forward.z*0.01f*playerSpeed);
         }
     }
 }
